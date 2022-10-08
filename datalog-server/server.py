4513e21c9d7931172cad8d571349a43786224fd5
Dax Harris <matteovh@gmail.com>
2022-10-08 18:11:58 -0400

    server works

--- datalog-server/server.py
+++ datalog-server/server.py
@@ -1,10 +1,10 @@
-from typing import Any, Dict
+from datetime import datetime
+from typing import Any, List, Optional
 from starlite import Starlite, post, get, Request, BaseRouteHandler, NotAuthorizedException
-from starlite.types import ASGIApp, Scope, Receive, Send
 from starlite.datastructures import State
 from dotenv import load_dotenv
 import os
-from tinydb import TinyDB
+from tinydb import TinyDB, where
 from pydantic import BaseModel
 import time
 
@@ -19,18 +19,33 @@ def guard_key(request: Request, _: BaseRouteHandler) -> None:
 
 def log(db: TinyDB, sensor: str, data: Any, log_time: float):
     db.insert({
+        "sensor": sensor,
         "logged_at": log_time,
         "received_at": time.time(),
         "data": data
     })
 
-
-def middleware_factory(app: ASGIApp) -> ASGIApp:
-    async def my_middleware(scope: Scope, receive: Receive, send: Send) -> None:
-        print(scope.items())
-        await app(scope, receive, send)
-
-    return my_middleware
+def get_from_db(db: TinyDB, sensors: List[str], before: datetime, after: datetime):
+    if (not sensors and not before and not after):
+        return db.all()
+    q = []
+    if sensors:
+        q.append('where("sensor").one_of(sensors)')
+    if before:
+        q.append('where("logged_at") <= before')
+    if after:
+        q.append('where("logged_at") >= after')
+    
+    return eval(
+        f'db.search({" & ".join(q)})', 
+        {
+            "sensors": sensors if sensors else [], 
+            "before": before.timestamp() if before else 0, 
+            "after": after.timestamp() if after else 0, 
+            "db": db,
+            "where": where
+        }
+    )
 
 class DataModel(BaseModel):
     logged_at: float
@@ -40,6 +55,10 @@ class DataModel(BaseModel):
 def post_data(sensor: str, data: DataModel, state: State) -> None:
     log(state.db, sensor, data.data, log_time=data.logged_at)
 
+@get("/data")
+def get_data(state: State, sensors: Optional[List[str]] = None, before: Optional[datetime] = None, after: Optional[datetime] = None) -> List[Any]:
+    return get_from_db(state.db, sensors, before, after)
 
 
-app = Starlite(route_handlers=[post_data], on_startup=[start], middleware=[middleware_factory])
+
+app = Starlite(route_handlers=[post_data, get_data], on_startup=[start])
